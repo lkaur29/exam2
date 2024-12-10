412f8b6f4b781aba501144d45edfa05eec404318
lkaur29 <lkaur5539@gmail.com>
2024-12-10 01:08:25 +0000

    html select

--- app/final/page.tsx
+++ app/final/page.tsx
@@ -5,6 +5,14 @@ export default function Page() {
       <form>
         <label htmlFor="product" className='ml-5'>Product Description</label>
         <br />
+        <label for="cars">Choose a car:</label>
+
+<select name="cars" id="cars">
+  <option value="volvo">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="mercedes">Mercedes</option>
+  <option value="audi">Audi</option>
+</select>
         <input type="text" name="product" id="product" className="ml-5 text-black bg-blue-400"/>
         <button className="m-5 focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900" type="submit">Save</button>
       </form>
