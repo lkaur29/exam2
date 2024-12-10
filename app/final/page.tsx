4a3dd39ad2ade7baa7496f82583115a330787e21
lkaur29 <lkaur5539@gmail.com>
2024-12-10 01:25:20 +0000

    style

--- app/final/page.tsx
+++ app/final/page.tsx
@@ -6,8 +6,8 @@ export default function Page() {
       <form>
         <label htmlFor="product" className='ml-5'>Product Description</label>
         <br />
-        <label htmlFor="cars">Choose a car:</label>
-
+        <label htmlFor="cars" className="mxl-5">Choose a car:</label>
+    
         <select name="cars" id="cars">
         <option value="volvo">Volvo</option>
         <option value="saab">Saab</option>
@@ -15,16 +15,29 @@ export default function Page() {
         <option value="audi">Audi</option>
         </select>
      
+        <br />
+        
+        <div className="ml-6 mt-6">
         <input type="radio" id="html" name="fav_language" value="HTML" />
         <label htmlFor="html">HTML</label><br />
          <input type="radio" id="css" name="fav_language" value="CSS" />
-         <label htmlFor="css">CSS</label><br>
-         <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+         <label htmlFor="css">CSS</label><br />
+         <input type="radio" id="javascript" name="fav_language" value="JavaScript" />
          <label htmlFor="javascript">JavaScript</label>
         <input type="text" name="product" id="product" className="ml-5 text-black bg-blue-400"/>
-      
-      
+        </div>
+
+        <div className="ml-2 mt-2">
+        <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+        <label htmlFor="vehicle1"> I have a bike</label><br />
+        <input type="checkbox" id="vehicle2" name="vehicle2" value="Car" />
+        <label htmlFor="vehicle2"> I have a car</label><br />
+        </div>
+        
+        <br/ >
+
         <button className="m-5 focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900" type="submit">Save</button>
+     
       </form>
       </>
     )
