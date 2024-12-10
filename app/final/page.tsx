b9e418b238bf3b13e392f5ce40c4b457c23f5af8
lkaur29 <lkaur5539@gmail.com>
2024-12-10 00:58:09 +0000

    Line break and m

--- app/final/page.tsx
+++ app/final/page.tsx
@@ -3,9 +3,9 @@ export default function Page() {
       <>
       <h1 className="text-3xl ml-5"> Final Exam</h1>
       <form>
-        <label htmlFor="product">Product Description</label>
-        <br/>
+        <label htmlFor="product" className='ml-5'>Product Description</label>
         <input type="text" name="product" id="product" className="m-5 text-black bg-blue-400"/>
+        <br />
         <button className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900" type="submit">Save</button>
       </form>
       </>
