b517f6a023bc8fca8a03e5ef77ac0c29477f33f7
lkaur29 <lkaur5539@gmail.com>
2024-12-10 00:56:51 +0000

    margin

--- app/final/page.tsx
+++ app/final/page.tsx
@@ -1,11 +1,11 @@
 export default function Page() {
     return (
       <>
-      <h1 className="text-3xl"> Final Exam</h1>
+      <h1 className="text-3xl ml-5"> Final Exam</h1>
       <form>
         <label htmlFor="product">Product Description</label>
         <br/>
-        <input type="text" name="product" id="product" className="text-black bg-blue-400"/>
+        <input type="text" name="product" id="product" className="m-5 text-black bg-blue-400"/>
         <button className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900" type="submit">Save</button>
       </form>
       </>
